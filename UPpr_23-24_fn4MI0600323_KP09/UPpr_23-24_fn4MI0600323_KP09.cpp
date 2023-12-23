06025dab70f2869aea14a25c9dc26f4f8633c9a5
BorisIvanov24 <boris.ivanov.go@gmail.com>
2023-12-23 02:01:14 +0200

    Added function for loading user information from file

--- UPpr_23-24_fn4MI0600323_KP09/UPpr_23-24_fn4MI0600323_KP09.cpp
+++ UPpr_23-24_fn4MI0600323_KP09/UPpr_23-24_fn4MI0600323_KP09.cpp
@@ -1,10 +1,107 @@
-// UPpr_23-24_fn4MI0600323_KP09.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
 
+constexpr unsigned MAX_LEN = 64;//maximum length of one line in the file
+unsigned usersLength;
+char** users;
+
+int convertCharToDigit(char ch)
+{
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';
+    return -1;
+}
+
+unsigned myAtoi(const char* str)
+{
+    if (!str)
+        return 0;
+
+    unsigned result = 0;
+    while (*str)
+    {
+        int digit = convertCharToDigit(*str);
+        if (digit == -1)
+            return 0;
+        (result *= 10) += digit;
+        str++;
+    }
+    return result;
+}
+
+void myStrcpy(const char* source, char* dest)
+{
+    if (!source || !dest)
+        return;
+    while (*source)
+    {
+        *dest = *source;
+        dest++;
+        source++;
+    }
+    *dest = '\0';
+}
+
+unsigned myStrlen(const char* str)
+{
+    if (!str)
+        return 0;
+
+    unsigned result = 0;
+    while (*str)
+    {
+        result++;
+        str++;
+    }
+    return result;
+}
+
+void loadUsers(const char* source)
+{
+    if (!source)
+    {
+        return;
+    }
+
+    std::ifstream sourceFile(source);
+    
+    if (!sourceFile.is_open())
+    {
+        std::cout << "Couldn't open file : " << source << std::endl;
+        return;
+    }
+
+    char temp[MAX_LEN] = "";
+    sourceFile.getline(temp, MAX_LEN);
+
+    usersLength = 2 * myAtoi(temp);
+    users = new char* [usersLength];
+    unsigned index = 0;
+
+    while (sourceFile)
+    {
+        sourceFile.getline(temp, MAX_LEN);
+
+        unsigned len = myStrlen(temp);
+        if (len != 0)
+        {
+            users[index] = new char[len+1];//len+1 because of '\0'
+            myStrcpy(temp, users[index]);
+            index++;
+        }
+    }
+
+    sourceFile.clear();
+    sourceFile.close();
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    loadUsers("Users.txt");
+    for (unsigned i = 0; i < usersLength; i++)
+        std::cout << users[i] << std::endl;
+
+    for (unsigned i = 0; i < usersLength; i++)
+        delete[] users[i];
+    delete[] users;
 }
 
